efd21b94f8131ef2265f563d88cb36a4b017e700
Colin Smith <colinsmith@colins-macbook-pro.local>
2015-10-18 22:00:48 -0400

    Initial Commit

---
+++ NSScreencastNSOperation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSScreencastNSOperation
+//
+//  Created by Colin Smith on 10/18/15.
+//  Copyright © 2015 Colin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSScreencastNSOperation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSScreencastNSOperation
+//
+//  Created by Colin Smith on 10/18/15.
+//  Copyright © 2015 Colin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
